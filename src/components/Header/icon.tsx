4743a1120a35273e594dd5924aa89cff8b552212
noodlewavey <117111580+noodlewavey@users.noreply.github.com>
2023-09-12 15:37:58 -0400

    my site with rotated animation box

--- src/components/Header/icon.tsx
+++ src/components/Header/icon.tsx
@@ -3,7 +3,20 @@ import Image from "next/image";
 import image from "../../tumblr_luda3yYxzm1qzt4vjo1_500_o.gif";
 
 export const IconDown = () => (
-  <div>
+  //   When you rotate an element counter-clockwise by 90 degrees:
+
+  // The original "top" of your element is now visually on the "left".
+  // The original "right" side of your element is now visually the "top".
+  // The original "bottom" is now visually on the "right".
+  // The original "left" side of your element is now visually the "bottom".
+  <div
+    style={{
+      transform: "rotate(-90deg)",
+      position: "relative",
+      left: "18rem", //increasing this shifts to the left..
+      top: "-20rem",
+    }}
+  >
     {/* Render the image */}
     <Image src={image} alt="Description of the image" />
   </div>
